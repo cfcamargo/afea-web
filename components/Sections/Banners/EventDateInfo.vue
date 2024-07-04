235da783e9b74d937c8a93e1adc651280bfc90ad
Cristian Camargo <contato@camargodev.com.br>
2024-07-03 23:04:57 -0400

    feat: adiciona banner de info

--- components/Sections/Banners/EventDateInfo.vue
+++ components/Sections/Banners/EventDateInfo.vue
@@ -3,7 +3,7 @@
         v-show="props.show"
         class="w-full h-dvh fixed top-0 bottom-0 left-0 right-0 bg-black/60 z-50 flex justify-center items-center" 
     >
-        <div class="xs:w-[90%] md:w-[60%] h-[30%] bg-white relative">
+        <div class="xs:w-[90%] md:w-[60%] max-w-[1920px] h-[30%] max-h-[1080px] bg-white relative">
             <button 
                 @click="close"
                 type="button" 
