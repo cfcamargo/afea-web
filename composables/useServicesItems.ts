565b899c3f2e76d10a7274b95ea60c2ce3a8c36e
Cristian <contato@camargodev.com.br>
2024-12-14 18:35:19 -0400

    fix: ajusta acessoria para assessoria

--- composables/useServicesItems.ts
+++ composables/useServicesItems.ts
@@ -8,87 +8,87 @@ export function useServicesItems(): servicesProps[] {
     const serviceProps:servicesProps[] = [
         {
             icon: 'IconAcessoriaRecepcao',
-            title: 'Acessoria de Recepção',
+            title: 'Assessoria de Recepção',
             link: 'https://chat.whatsapp.com/CH64W2ByVdyAtRmAZVlChr'
         },
         {
             icon: 'IconIdentificacaoECredenciamento',
-            title: 'Acessoria de Identificação e credenciamento',
+            title: 'Assessoria de Identificação e credenciamento',
             link: 'https://chat.whatsapp.com/IL3Ila2A1hbG6RJsRpwj5k'
         },
         {
             icon: 'IconAcessoriaJuridica',
-            title: 'Acessoria Jurídica Internacional',
+            title: 'Assessoria Jurídica Internacional',
             link: 'https://chat.whatsapp.com/EYBu8lcpu2JDa81ZAPYC0s'
         },
         {
             icon: 'IconAcessoriaImobiliaria',
-            title: 'Acessoria Imobiliária',
+            title: 'Assessoria Imobiliária',
             link: 'https://chat.whatsapp.com/FaR6d76r8kD1utvKuw4SKH'
         },
         {
             icon: 'IconAcessoriaMobiliaria',
-            title: 'Acessoria Mobiliária',
+            title: 'Assessoria Mobiliária',
             link: 'https://www.instagram.com/desapegueaquidafronteira?igsh=aGc2cGNtaXR6Y3dz&utm_source=qr'
         },
         {
             icon: 'IconAcessoriaManutencaoMobiliaria',
-            title: 'Acessoria de Manutanção Residencial',
+            title: 'Assessoria de Manutanção Residencial',
             link: 'https://chat.whatsapp.com/H0Ge9k2g1RuI6GBGmHiBTa'
         },
         {
             icon: 'IconAcessoriaMobilidade',
-            title: 'Acessoria de Mobilidade Urbana',
+            title: 'Assessoria de Mobilidade Urbana',
             link: 'https://chat.whatsapp.com/CeuRnSmzbGfGIJj50otOW7'
         },
         {
             icon: 'IconAcessoriaFraternal',
-            title: 'Acessoria Fraternal',
+            title: 'Assessoria Fraternal',
             link: 'https://chat.whatsapp.com/H8OpoB4YLZjK3slCyGgbEs'
         },
         {
             icon: 'IconAcessoriaEmocional',
-            title: 'Acessoria Emocional Psicológica',
+            title: 'Assessoria Emocional Psicológica',
             link: 'https://chat.whatsapp.com/LLjKu0gEEW8298zFnae56f'
         },
         {
             icon: 'IconAcessoriaDidatica',
-            title: 'Acessoria Didática',
+            title: 'Assessoria Didática',
             link: 'https://chat.whatsapp.com/CP1adhG2AjPDrFQFkio5fw'
         },
         {
             icon: 'IconOportunity',
-            title: 'Acessoria de Oportunidades de Trabalho',
+            title: 'Assessoria de Oportunidades de Trabalho',
             link: 'https://chat.whatsapp.com/DX524KazR9bIGtCktIZ7WP'
         },
         {
             icon: 'IconAcessoriaMarketing',
-            title: 'Acessoria de Marketing',
+            title: 'Assessoria de Marketing',
             link: 'https://chat.whatsapp.com/InpqZPmVeWnGng2emwYtc7'
         },
         {
             icon: 'iconDivulgacao',
-            title: 'Acessoria de Divulgação',
+            title: 'Assessoria de Divulgação',
             link: 'https://chat.whatsapp.com/BrCEXtnMaU3GPoPmOsKsBk'
         },
         {
             icon: 'IconAcessoriaEventos',
-            title: 'Acessoria em Eventos',
+            title: 'Assessoria em Eventos',
             link: 'https://chat.whatsapp.com/Lh38YZWuAZk7UtZndxlB47'
         },
         {
             icon: 'IconAcessoriaViagem',
-            title: 'Acessoria de Viagens',
+            title: 'Assessoria de Viagens',
             link: 'https://chat.whatsapp.com/Ingk1FZfzQ3AcauBPZNPF9'
         },
         {
             icon: 'IconAcessoriaFormatura',
-            title: 'Acessoria de Formatura',
+            title: 'Assessoria de Formatura',
             link: 'https://chat.whatsapp.com/IDjI2hBk1DC6yg8rMw6xB4'
         },
         {
             icon: 'IconAcessoriaFinanceira',
-            title: 'Acessoria em Finanças',
+            title: 'Assessoria em Finanças',
             link: 'https://chat.whatsapp.com/JrwzvsRUrQCCHkUyiKKhj7'
         },
         {
